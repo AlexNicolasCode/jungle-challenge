db74fbbad21b365858d8711fe3692d4cdd55d2b2
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-09 01:15:33 -0300

    feat: update PaginationInput to enable set min and max size to avoid fetch all tasks from database and throws

--- tasks-service/src/shared/dtos/pagination.input.ts
+++ tasks-service/src/shared/dtos/pagination.input.ts
@@ -1,12 +1,15 @@
 import { Transform } from 'class-transformer';
-import { IsNumber } from 'class-validator';
+import { IsNumber, Max, Min } from 'class-validator';
 
 export class PaginationInput {
   @IsNumber()
+  @Min(1)
+  @Max(100)
   @Transform(({ value }) => Number(value), { toClassOnly: true })
   size: number;
 
   @IsNumber()
+  @Min(1)
   @Transform(({ value }) => Number(value), { toClassOnly: true })
   page: number;
 }
