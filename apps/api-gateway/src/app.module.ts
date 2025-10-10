4426ea869fedcc0889b5a723348cc210624a0cd5
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-10 05:51:22 -0300

    feat: add rate limit in api gateway

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule, TaskModule } from './modules';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, TaskModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TaskModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
