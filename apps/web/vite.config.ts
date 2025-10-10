2b0fdf2d6ea15b2eb769adf0aea78d6795ae9a6a
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-10 02:21:51 -0300

    chore: integrate tailwind css in web project

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
+    tailwindcss(),
     react(),
   ],
 })
