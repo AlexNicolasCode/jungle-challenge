0d5864292a592c434be206dae1be27dc33f1d04e
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-11 07:21:20 -0300

    feat: enable interceptor in task axios client to refresh token and retry when api return 401 status code

--- apps/web/src/clients/tasks/index.ts
+++ apps/web/src/clients/tasks/index.ts
@@ -1,10 +1,44 @@
 import axios from "axios";
 
-import { getAccessToken } from "../../shared/utils";
+import { authApiClient } from "../auth";
 
-export const taskApiClient = axios.create({
-    baseURL: 'http://localhost:3000/api/tasks',
-    headers: {
-        Authorization: `Bearer ${getAccessToken()}`,
-    }
-})
+const taskApiClient = axios.create({
+        baseURL: 'http://localhost:3000/api/tasks',
+    });
+
+taskApiClient.interceptors.request.use(
+        (config) => {
+            const storagedTokens = localStorage.getItem('tokens');
+            if (!storagedTokens) {
+                return config;
+            }
+            const { accessToken } = JSON.parse(storagedTokens);
+            if (accessToken) {
+                config.headers.Authorization = `Bearer ${accessToken}`;
+            }
+            return config;
+        },
+        async (error) => {
+            const statusCode = error.response?.data?.statusCode;
+            const isAuthenticatedError = statusCode === 401;
+            const storagedTokens = localStorage.getItem('tokens');
+            if (!isAuthenticatedError || !storagedTokens) {
+                return Promise.reject(error);
+            }
+            const { refreshToken } = JSON.parse(storagedTokens);
+            const response = await authApiClient.post('/refresh', { refreshToken });
+            const accessToken = response.data.accessToken;
+            localStorage.setItem('tokens', JSON.stringify({
+                ...response.data,
+                refreshToken,
+            }))
+            return taskApiClient.request({
+                ...error.config,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                }
+            });
+        }
+    );
+
+export { taskApiClient };
