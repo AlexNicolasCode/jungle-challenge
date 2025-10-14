9400d0bd7e6e37cb325512f078292100867cccb0
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-14 15:05:25 -0300

    feat: update styles to use navigation menu from shadcn

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { createRootRoute, Link, Outlet, useNavigate } from '@tanstack/react-rout
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { useEffect } from 'react';
 
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu';
 import { useAuth, useNotification } from '../hooks';
 
 const RootLayout = () => {
@@ -18,33 +19,46 @@ const RootLayout = () => {
 
   const renderAuthenticatedTopBar = () => {
     return (
-      <>
-        <Link to="/" className="[&.active]:font-bold mr-2">
-        Home
-        </Link>
-        <Link to="/tasks/create" className="[&.active]:font-bold mr-2">
-          Create Task
-        </Link>
-        <button onClick={() => {
-            logout();
-            navigate({ to: '/login' });
-        }} className="[&.active]:font-bold">
-          Logout
-        </button>
-      </>
+      <NavigationMenu>
+        <NavigationMenuList>
+            <NavigationMenuItem>
+                <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink asChild>
+                        <Link to="/">Home</Link>
+                    </NavigationMenuLink>
+                    <NavigationMenuLink asChild>
+                        <Link to="/tasks/create">Create Task</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <NavigationMenuLink onClick={logout}>
+                    Logout
+                </NavigationMenuLink>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     )
   }
 
   const renderUnauthenticatedTopBar = () => {
     return (
-      <>
-        <Link to="/login" className="[&.active]:font-bold">
-          Login
-        </Link>
-        <Link to="/register" className="[&.active]:font-bold">
-          SignUp
-        </Link>
-      </>
+      <NavigationMenu>
+        <NavigationMenuList>
+            <NavigationMenuItem>
+            <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <NavigationMenuLink asChild>
+                        <Link to="/login">Login</Link>
+                    </NavigationMenuLink>
+                    <NavigationMenuLink asChild>
+                        <Link to="/register">Register</Link>
+                    </NavigationMenuLink>
+                </NavigationMenuContent>
+            </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     )
   }
 
