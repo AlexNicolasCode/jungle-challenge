7dcf030c0eb8ea5c0222caf6d02f91875d8c0693
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-10-17 17:40:48 -0300

    fix: set correct path to load migrations

--- apps/auth-service/src/database/config/database.config.ts
+++ apps/auth-service/src/database/config/database.config.ts
@@ -1,17 +1,18 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 export const dbConfig = TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
-      host: configService.get('DB_HOST', 'db'),
-      port: configService.get<number>('DB_PORT', 5432),
-      username: configService.get('DB_USER', 'postgres'),
-      password: configService.get('DB_PASSWORD', 'password'),
-      database: configService.get('DB_NAME', 'auth_db'),
-      entities: ['dist/database/entities/*.entity.js'],
-      migrations: ['dist/database/migrations/*.js'],
-      migrationsRun: true,
-    }),
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST', 'db'),
+    port: configService.get<number>('DB_PORT', 5432),
+    username: configService.get('DB_USER', 'postgres'),
+    password: configService.get('DB_PASSWORD', 'password'),
+    database: configService.get('DB_NAME', 'auth_db'),
+    entities: [join(__dirname, '../entities/**/*.entity{.ts,.js}')],
+    migrations: [join(__dirname, '../migrations/*{.ts,.js}')],
+    migrationsRun: true,
+  }),
 });
